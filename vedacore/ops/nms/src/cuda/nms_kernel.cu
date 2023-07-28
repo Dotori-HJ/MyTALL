ef24f90f86c0e11aa89194183d435672490dd70f
Ho-Joong Kim <hojoong_kim@korea.ac.kr>
2023-07-28 09:37:56 +0900

    f

--- vedacore/ops/nms/src/cuda/nms_kernel.cu
+++ vedacore/ops/nms/src/cuda/nms_kernel.cu
@@ -87,14 +87,15 @@ at::Tensor nms_cuda_forward(const at::Tensor segments, float nms_overlap_thresh)
 
   // THCState *state = at::globalContext().lazyInitCUDA(); // TODO replace with getTHCState
 
-  // unsigned long long* mask_dev = NULL;
-  //THCudaCheck(THCudaMalloc(state, (void**) &mask_dev,
+  unsigned long long* mask_dev = NULL;
+  // THCudaCheck(THCudaMalloc(state, (void**) &mask_dev,
   //                      segments_num * col_blocks * sizeof(unsigned long long)));
 
   // mask_dev = (unsigned long long*) THCudaMalloc(state, segments_num * col_blocks * sizeof(unsigned long long));
   // at::Tensor mask_dev = at::empty({segments_num * col_blocks}, at::device(at::kCUDA).dtype(at::kLong));
 
-  mask_dev = (unsigned long long*) AT_CUDA_CHECK(at::cudaMalloc(state, segments_num * col_blocks * sizeof(unsigned long long)));
+  // mask_dev = (unsigned long long*) AT_CUDA_CHECK(at::cudaMalloc(state, segments_num * col_blocks * sizeof(unsigned long long)));
+  mask_dev = (unsigned long long*) AT_CUDA_CHECK(c10::cuda::CUDACachingAllocator::raw_alloc(segments_num * col_blocks * sizeof(unsigned long long)));
 
   dim3 blocks(at::ceil_div(segments_num, threadsPerBlock),
               at::ceil_div(segments_num, threadsPerBlock));
